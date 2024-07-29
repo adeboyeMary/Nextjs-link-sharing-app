3accb6d5e1909d409945b37be803c1ccffadb7f7
Adeboye Mary <maryadeboye69@gmail.com>
2024-07-29 12:49:46 +0100

    Add login and signin page

---
+++ src/pages/createAccount.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link"
+import Image from "next/image"
+import devlink from "../asset/devlink.svg";
+
+export default function CreateAccount () {
+    return (
+    <main className="bg-white h-[100vh] md:bg-[#FAFAFA] lg:bg-[#FAFAFA]">
+      <div className="pt-[6rem] md:pt-[14rem] lg:pt-[2rem]  ">
+        <div className="flex flex-row gap-2 ml-5 lg:justify-center">
+            <Image src={devlink} alt="devlink" width={500} height={300} className="mb-[2.5rem] w-9" />
+            <h1 className="font-bold text-3xl md:text-center lg:text-center mb-[0.6rem] 
+            md:mb-[2rem] lg:mb-[2rem] ">devlinks</h1>
+        </div>
+
+        <div className="bg-white md:w-[60%] lg:w-[35%] m-auto p-8 md:rounded-xl lg:rounded-xl 
+        text-sm md:text-base lg:text-sm ">
+          <h1 className="font-bold text-3xl mb-2">Create Account</h1>
+          <p className="mb-6">Lets get you started sharing your links!</p>
+          <form className="flex flex-col text-[#737373] ">
+            <>
+              <label>Email address</label>
+              <input type="text"  className="w-[100%] py-[8px] lg:py-[7px] border-[1px] border-[#D9D9D9] 
+              rounded-lg text-xs" placeholder="e.g alex@email.com" required/>
+            </>
+            
+            <>
+              <label className="mt-3">Create Password</label>
+              <input type="text"  className="w-[100%] py-[8px] lg:py-[7px] border-[1px] border-[#D9D9D9] 
+              rounded-lg text-xs" placeholder="At least 8 characters" required/>
+            </>
+
+            <>
+              <label className="mt-3">Confirm Password</label>
+              <input type="text"  className="w-[100%] py-[8px] lg:py-[7px] border-[1px] border-[#D9D9D9] 
+              rounded-lg text-xs" placeholder="At least 8 characters" required/>
+            </>
+            <p className="mt-4">Password must contain at least 8 characters</p>
+            <button className="bg-[#633BFF] text-white my-4 py-[7px] rounded-lg ">Create new account</button>
+          </form>
+            <p className="text-center">Do not have an account? 
+                <Link href='/' className="text-[#633BFF] "> Login</Link></p>
+        </div>
+      </div>
+    </main>
+    )
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,40 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
+import devlink from "../asset/devlink.svg";
+import email from "../asset/email.svg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={` bg-white h-[100vh] md:bg-[#FAFAFA] lg:bg-[#FAFAFA] ${inter.className}`}>
+      <div className="pt-[6rem] md:pt-[14rem] lg:pt-[6rem]  ">
+        <div className="flex flex-row gap-2 ml-5 md:justify-center lg:justify-center ">
+            <Image src={devlink} alt="devlink" width={500} height={300} className="mb-[2.5rem] w-9" />
+            <h1 className="font-bold text-3xl md:text-center lg:text-center mb-[0.6rem] 
+            md:mb-[2rem] lg:mb-[2rem] ">devlinks</h1>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="bg-white md:w-[60%] lg:w-[35%] m-auto p-8 md:rounded-xl lg:rounded-xl text-sm md:text-base ">
+          <h1 className="font-bold text-3xl mb-2">Login</h1>
+          <p className="mb-5">Add your details below to get back into the app</p>
+          <form className="flex flex-col text-[#737373] ">
+            <>
+              <label>Email address</label>
+              <input className="w-[100%] py-[8px] lg:py-[7px] border-[1px] border-[#D9D9D9] rounded-lg text-sm" 
+              placeholder="e.g alex@email.com"  required />
+            </>
+            
+            <>
+              <label className="mt-3">Password</label>
+              <input className="w-[100%] py-[8px] lg:py-[7px] border-[1px] border-[#D9D9D9] rounded-lg text-sm"
+              placeholder="Enter your password" required />
+            </>
+            <button className="bg-[#633BFF] text-white my-4 py-[7px] rounded-lg ">Login</button>
+          </form>
+            <p className="text-center">Do not have an account? <Link href='/createAccount' className="text-[#633BFF] ">Create account</Link></p>
+        </div>
       </div>
     </main>
   );
